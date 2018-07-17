8a358864b44fcc070e932df3d7e7c81409f5f58b
Ryan Pierson <alhampton91@gmail.com>
2018-07-16 18:38:56 -0700

    footnotes

--- merge_docx/merge.py
+++ merge_docx/merge.py
@@ -10,8 +10,63 @@ from .utils.handle_sections import handle_sections
 from .utils.handle_headers_footers import handle_headers_footers
 from .utils.handle_footnotes import handle_footnotes
 
+
+# The first merge will be into a blank .docx file containing a footnote part.
+# This allows subsequently merged documents to preserve their footnotes.
+def blank_merge(file):
+    file_no_elem = file + 'no_elem.docx'
+    # Remove floating shapes and hyperlinks
+    handle_floats(file, file_no_elem)
+    handle_hyperlinks(file_no_elem, file_no_elem)
+
+    # blank.docx contains the necessary footnote part, so this is the template
+    merged_document = Document('templates/blank.docx')
+
+    # The document to be merged in exists as the second file in the list. Note
+    # that this script only merges 2 files at a time.
+    sub_doc = Document(file_no_elem)
+
+    handle_footnotes(merged_document, sub_doc)
+
+    # Add each style in sub_styles to merged_styles.
+    handle_styles(merged_document, sub_doc)
+
+    # Remove the temporary files generated without floating elements. If a file
+    # is being merged into itself, there will only be one document generated 
+    # without floats/hyperlinks. Trying to delete both documents will throw an exception,
+    # but I don't care as long as the file gets deleted. 
+    try:
+        os.remove(file_no_elem)
+    except OSError:
+        pass
+
+    # Add the inline images from sub_doc into the merged document
+    handle_inlines(merged_document, sub_doc)
+
+    # Merge the document bodies   
+    for element in sub_doc.element.body:
+        merged_document.element.body.append(element)
+
+    # Regroup end-sections lost in the merge.
+    handle_sections(merged_document)
+
+    # Remove all header and footer references.
+    handle_headers_footers(merged_document)
+
+    # Save the altered file
+    destination = 'temp.docx'
+
+    merged_document.save(destination)
+
+    # Return the file location to be used in the next merge
+    return destination
+
+
+
 # Merge 'file' into 'template'
 def merge_docx(template, file, destination):
+
+    template = blank_merge(template)
 
     # Save with a .docx file extension
     if destination[-5:] != '.docx':
@@ -26,10 +81,20 @@ def merge_docx(template, file, destination):
     handle_hyperlinks(template_no_elem, template_no_elem)
     handle_hyperlinks(file_no_elem, file_no_elem)
 
+    # Template was overwritten as 'temp.docx' in blank_merge, but is not needed
+    # at this point because it has already been opened and processed
+    os.remove(template)
+
     # Use the first document as a template file. All formatting will be 
     # inherited from this file. All media and relationships from this file are
     # preserved.
     merged_document = Document(template_no_elem)
+
+    # Remove the blank paragraph created by blank_merge
+    blank_para = merged_document.paragraphs[0]
+    p = blank_para._element
+    p.getparent().remove(p)
+    p._p = p._element = None
 
     # The document to be merged in exists as the second file in the list. Note
     # that this script only merges 2 files at a time.
@@ -67,10 +132,10 @@ def merge_docx(template, file, destination):
     # Remove all header and footer references.
     handle_headers_footers(merged_document)
 
-    # Remove all footnote references.
-    #handle_footnotes(merged_document)
-
-
     # Save the altered file
     merged_document.save(destination)
+
+
+
+
     
--- merge_docx/utils/handle_footnotes.py
+++ merge_docx/utils/handle_footnotes.py
@@ -2,71 +2,120 @@ import docx
 from docx.opc.constants import RELATIONSHIP_TYPE as RT
 from lxml import etree
 
-def iter_footnote_parts(doc):
+NS = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+
+# Return the content corresponding to a documents footnotes.xml file
+def get_footnote_part(doc):
     for rel in doc.part.rels.values():
         if rel.reltype == RT.FOOTNOTES:
-            yield rel.target_part
+            return rel.target_part
 
 
+# Get the highest footnote id present in a given document.
 def get_highest_footnote_id(doc):
     highest_id = 0
 
-    ns = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+    # Get docs's footnotes.xml file
+    footnote_part = get_footnote_part(doc)
+    footnote_xml = footnote_part._blob
 
-    for footnote_part in iter_footnote_parts(doc):
-        footnote_xml = footnote_part._blob
+    root = etree.fromstring(footnote_xml)
 
-        root = etree.fromstring(footnote_xml)
-
-        children = list(root)
-        for child in children:
-            x = int(child.get(ns + 'id'))
-            if x > highest_id:
-                highest_id = x
+    children = list(root)
+    for child in children:
+        id_num = int(child.get(NS + 'id'))
+        if id_num > highest_id:
+            highest_id = id_num
 
     return highest_id
 
 
-def get_sub_footnotes(doc):
-    highest_id = get_highest_footnote_id(template)
+# Return a list of footnote elements in a given document.
+def get_footnotes(doc):
+    footnotes = []
 
-    ns = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
-
-    for footnote_part in iter_footnote_parts(sub_doc):
+    # Get docs's footnotes.xml file
+    footnote_part = get_footnote_part(doc)
+    if footnote_part:
         footnote_xml = footnote_part._blob
 
         root = etree.fromstring(footnote_xml)
 
         children = list(root)
         for child in children:
-            if int(child.get(ns + 'id')) > 0:
-                # Append it to templates footnotes.xml
-                print(child)
+            if int(child.get(NS + 'id')) > 0:
+                footnotes.append(child)
+
+        return footnotes
+    else:
+        return None
 
 
+# Update each footnote id to avoid a conflict with footnotes already 
+# existing in template's footnotes.xml file.
+def update_footnote_ids(footnotes, highest_id):
+    # For each footnote starting at 1, re-number starting at highest_id
+    for footnote in footnotes:
+        id_num = int(footnote.get(NS + 'id'))
+        id_num += highest_id
+        footnote.set(NS + 'id', str(id_num))
 
+    return footnotes
+
+
+# Update all of the documents footnote references to match the updated ids 
+# generated in update_footnote_ids(footnotes, highest_id).
+def update_document_footnotes(doc, highest_id):
+    for para in doc.paragraphs:
+        for run in para.runs:
+            for elem in run.element.getchildren():
+                if elem.tag == NS + 'footnoteReference':
+                    id_num = int(elem.get(NS + 'id'))
+                    id_num += highest_id
+                    elem.set(NS + 'id', str(id_num))
 
 
 # Remove all references to headers and footers.
 def handle_footnotes(template, sub_doc):
+    # Get template's footnotes.xml file
+    footnote_part = get_footnote_part(template)
+
+    # This list will remain empty if sub_doc has no footnotes
+    updated_footnotes = []
+
+    # Get the highest footnote id in the template document. This will be used
+    # to ensure that any new footnotes inserted into the document do not share
+    # an id with an already existing footnote.
     highest_id = get_highest_footnote_id(template)
 
+    # Return a list of footnote elements in sub_doc
+    sub_footnotes = get_footnotes(sub_doc)
 
-    ns = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+    if sub_footnotes:
 
-    for footnote_part in iter_footnote_parts(sub_doc):
-        footnote_xml = footnote_part._blob
+        # Update sub_docs footnote ids
+        updated_footnotes = update_footnote_ids(sub_footnotes, highest_id)
 
-        root = etree.fromstring(footnote_xml)
+        # Update sub_docs footnote references
+        update_document_footnotes(sub_doc, highest_id)
 
-        children = list(root)
-        for child in children:
-            if int(child.get(ns + 'id')) > 0:
-                # Append it to templates footnotes.xml
-                print(child)
 
-        new_footnote_xml = footnote_xml
+    footnote_xml = footnote_part._blob
 
-        footnote_part._blob = new_footnote_xml
+    # Convert footnote_xml into etree elements for processing
+    root = etree.fromstring(footnote_xml)
+        
+    # Append sub_docs updated footnotes to root's footnotes element
+    for footnote in updated_footnotes:
+        root.append(footnote)
+
+    # Serialize the xml to update the document
+    new_footnote_xml = etree.tostring(root)
+
+    # Overwrite the old footnotes.xml file
+    footnote_part._blob = new_footnote_xml
+
+
+    
 
 
